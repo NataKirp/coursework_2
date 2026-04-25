0127938c2c56a2bed0a631c24b05bfa7b781adf4
Natalya Kirpichnikova <kirpichnikova.nb@gmail.com>
2026-04-25 20:46:01 +0300

    Доработаны функции в api.py

--- src/api.py
+++ src/api.py
@@ -1,77 +1,79 @@
 from abc import ABC, abstractmethod
+from typing import Optional
 
-from requests import get, RequestException
+import requests
+from requests import RequestException
 
 
-class AeroplanesAPI(ABC):
+class APIAdapter(ABC):
     """Абстрактный класс для работы с API"""
 
     @abstractmethod
-    def connect_to_api(self, country: str) -> None:
-        """Метод для установки соединения с API"""
+    def _connect_to_api(self, url: str, params: dict[str, any], headers: Optional[dict[str, str]]) -> requests.Response:
+        """Абстрактный метод для установки соединения с API"""
         pass
 
     @abstractmethod
     def get_aeroplanes(self, country: str) -> None:
+        """Абстрактный метод для получения информации о самолетах"""
         pass
 
 
-class APIAdapter(AeroplanesAPI):
+class AeroplanesAPI(APIAdapter):
+    """Класс для получения данных о полетах с сервисов nominatim.openstreetmap.org и opensky-network.org."""
 
     def __init__(self) -> None:
-        self.__aeroplanes = None
+        """Инициализация приватных атрибутов класса и хранилища."""
         self.__openstreetmap_url = 'https://nominatim.openstreetmap.org/search'
         self.__opensky_url = 'https://opensky-network.org/api/states/all?'
+        self.aeroplanes = None
 
-    def __get_geo_coordinates(self, country: str) -> dict:
+    def _connect_to_api(self, url: str, params: dict[str, any], headers: Optional[dict[str, str]]) -> requests.Response:
+        """Метод подключения к API с проверкой статус-кода ответа."""
+        try:
+            response = requests.get(url=url, params=params, headers=headers)
+            response.raise_for_status()  # Вызовет ошибку, если статус не 200
+            return response
+        except RequestException as e:
+            raise Exception(f'Ошибка доступа к API: {str(e)}')
+
+    def get_geo_coordinates(self, country: str) -> dict:
         """Метод подключения к API для получения географических координат страны"""
         headers_nominatim = {
             'User-Agent': 'test-app/1.0',
         }
-        # Указываем параметры: в каком формате возвращать данные и максимальную длину списка стран в ответе.
+        # указываем параметры: в каком формате возвращать данные и максимальную длину списка стран в ответе.
         params_nominatim = {
             'country': country,
             'format': 'json',
             'limit': 1,
         }
-        try:
-            response_nominatim = get(url=self.__openstreetmap_url, params=params_nominatim, headers=headers_nominatim)
-            response_nominatim.raise_for_status()  # Вызовет ошибку, если статус не 200
-            data = response_nominatim.json()
-            return data[0].get('boundingbox')
-        except RequestException as e:
-            raise Exception(f'Не удалось получить гео координаты страны: {str(e)}')
+        response_nominatim = self._connect_to_api(self.__openstreetmap_url, params_nominatim, headers_nominatim)
+        data = response_nominatim.json()
 
-    def __filter_aeroplanes(self, geo_coordinates: list) -> dict:
+        if not data:
+            print(f'Страна {country} не найдена')
+
+        return data[0].get('boundingbox')
+
+    def filter_aeroplanes(self, geo_coordinates: list) -> dict:
         """Метод подключения к API для получения данных о самолетах, находящихся в заданных координатах"""
-
-        # Параметры для фильтрации самолетов по их географическим координатам.
+        # параметры для фильтрации самолетов по их географическим координатам.
         params_opensky = {
             'lamin': geo_coordinates[0],
             'lamax': geo_coordinates[1],
             'lomin': geo_coordinates[2],
             'lomax': geo_coordinates[3],
         }
-        try:
-            response_opensky = get(url=self.__opensky_url, params=params_opensky)
-            response_opensky.raise_for_status()  # Вызовет ошибку, если статус не 200
-            return response_opensky.json()
-        except RequestException as e:
-            raise Exception(f'Не удалось получить перечень самолетов: {str(e)}')
+        response_opensky = self._connect_to_api(self.__opensky_url, params_opensky, None)
+        return response_opensky.json()
 
     def get_aeroplanes(self, country: str) -> None:
         """Метод получения данных о самолетах, находящихся в воздушном пространстве страны"""
-        self.connect_to_api(country)
+        country_coordinates = self.get_geo_coordinates(country)
+        self.aeroplanes = self.filter_aeroplanes(list(country_coordinates))
 
-    def connect_to_api(self, country: str) -> None:
-        """Метод подключения к API сервисов nominatim.openstreetmap.org и opensky-network.org"""
-
-        country_coordinates = self.__get_geo_coordinates(country)
-        self.__aeroplanes = self.__filter_aeroplanes(list(country_coordinates))
-
-
-if __name__ == '__main__':
-    api = APIAdapter()
-    api.get_aeroplanes('Canada')
-    # print(api.aeroplanes)
-    print(api._APIAdapter__aeroplanes)
+# if __name__ == '__main__':
+#     api = AeroplanesAPI()
+#     api.get_aeroplanes('Canada')
+#     print(api.aeroplanes)
